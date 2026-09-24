5284b91f3b53c81eac0ce08dfc3e803710b4b98c
thytabakman-jpg <thytabakman@gmail.com>
2026-09-24 11:05:40 -0400

    ICC executes adaptive program harness construction

---
+++ experiments/006-adaptive-program/mutations.py
@@ -0,0 +1,9 @@
+MUTATIONS = {
+ "unlock_goal": "target_preserved",
+ "hide_observation": "execution_truth",
+ "accept_without_ack": "execution_truth",
+ "terminate_without_verify": "verified_before_terminate",
+ "collapse_incomparable": "incomparable_preserved",
+ "drop_provenance": "provenance_preserved",
+ "ignore_authority": "authority_respected",
+}
